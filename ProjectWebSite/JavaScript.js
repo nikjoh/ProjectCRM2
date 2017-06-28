08d83a8c58682a3ea0c61960594806500e8569f9
JohannaCN <chingnyman@gmail.com>
2017-06-28 13:58:03 +0200

    uppdaterat med knapp

--- ProjectWebSite/JavaScript.js
+++ ProjectWebSite/JavaScript.js
@@ -1,1 +1,22 @@
-﻿
+﻿function AddContact() {
+ var firstName = document.getElementById("firstName").value;
+var lastName = document.getElementById("lastName").value;
+var ssn = document.getElementById("company").value;
+
+var myBody = document.getElementById("tableBody");
+var myTR = document.createElement("tr");
+
+var myTDFirstName = document.createElement("td");
+myTDFirstName.innerHTML = firstName;
+myTR.appendChild(myTDFirstName);
+
+var myTDLastName = document.createElement("td");
+myTDLastName.innerHTML = lastName;
+myTR.appendChild(myTDLastName);
+
+var my = document.createElement("td");
+myTDSSN.innerHTML = company;
+myTR.appendChild(myTDSSN);
+
+myBody.appendChild(myTR);
+}
