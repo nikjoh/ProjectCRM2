550b0074fa9ae8c4b35df4230cd170922777d765
Veronica Roeoes <roos.veronica@gmail.com>
2017-06-28 19:55:31 +0200

    Style uppdateringar

--- ProjectWebSite/JavaScript.js
+++ ProjectWebSite/JavaScript.js
@@ -41,3 +41,20 @@ function PrintContacts() {
 //function UpdateContact() {
 
 //}
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        //console.log($(window).scrollTop())
+
+        if ($(window).scrollTop() > 280) {
+            $('nav').addClass('navbar-fixed');
+        }
+        if ($(window).scrollTop() < 281) {
+            $('nav').removeClass('navbar-fixed');
+        }
+    });
+});
