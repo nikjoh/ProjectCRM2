30196d7233d3c6b77f9354a9f2c654b8b9835600
Petter Olgemar <petterolgemar_92@hotmail.com>
2017-06-28 17:14:51 +0200

    Diverse

--- ProjectWebSite/JavaScript.js
+++ ProjectWebSite/JavaScript.js
@@ -1,20 +1,36 @@
-﻿
+﻿AddContact(){
 
 
-function PrintContacts(){
+}
+
+//PrintContacts
+function PrintContacts() {
+    
     $.getJSON('http://localhost:62111/KAB.aspx').done(function (data) {
 
         $("#tableBody").children().remove();
 
         var stringToAppend = "";
         for (var i = 0; i < data.length; i++) {
-            stringToAppend += "<tr><td onclick='alert("+data[i].ID+");'>" + data[i].FirstName + "</td><td>" + data[i].LastName + "</td>" + "<td>" + data[i].Company + "</td>";
-
+            
+            stringToAppend += "<tr><td>" + data[i].FirstName + "</td><td>" + data[i].LastName + "</td>" + "<td>" + data[i].Company + "</td>";
+                    
             stringToAppend += "</tr>";
         }
         //stringToAppend += "";
 
         $("#tableBody").append(stringToAppend);
-
+        //$("#contactList").show();
     });
 }
+
+
+//function Hide() {
+    
+//    $("#contactList").hide();
+
+//}
+//function SortContacts() {
+
+
+//}
